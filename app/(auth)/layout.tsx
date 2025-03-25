9663ea949f0d68dba4e15e26e665061a963f3cfa
Alexiagpco25 <alexiagpco@gmail.com>
2025-03-24 23:10:21 -0600

    feat: JWT retrieved

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,23 +1,21 @@
 import Image from "next/image";
 
 export default function AuthLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>){
-    return(
-    <div className="bg-orange-200 w-screen h-screen overflow-hidden grid">
-    <div className="place-content-center place-self-center place-items-center text-center">
-        <div className="flex flex-col items-center bottom-10 relative">
-        <Image 
-        src="/Logo.svg"
-        alt="Logo de Ocso" 
-        width={250}  
-        height={0}>
-        </Image>
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="bg-orange-200 w-screen h-screen overflow-hidden grid place-items-center">
+      <div className="text-center flex flex-col items-center">
+        <div className="flex flex-col items-center mb-10">
+          <Image 
+            src="/Logo.svg"
+            alt="Logo de Ocso" 
+            width={250}  
+            height={0}
+          />
         </div>
         {children}
-        </div>
+      </div>
     </div>
-    );
+  );
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,26 +1,41 @@
+'use client';
 import { Input, Button } from "@nextui-org/react";
 import Link from "next/link";
 import axios from "axios";
+import { API_URL } from "@/constants";
 
 export default function LoginPage() {
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    const data = new FormData(e.target);
-    axios.post("")
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    let authData: any = {}
+    authData.userEmail = formData.get("userEmail");
+    authData.userPassword = formData.get("userPassword");
+    const {data} = await axios.post(`${API_URL}/auth/login`,{
+      ...authData
+    });
+    console.log(data);
     return;
   }
+
   return (
-    <form className="border border-orange-500 bg-orange-500 text-white px-10 py-2 rounded-md">
-      <p className="text-2xl my-4">Iniciar Sesión</p>
-      <div className="flex flex-col gap-2 my-4 items-center">
-        <Input label="Email"name="UserEmail" type="email" isRequired={true} size="sm" />
-        <Input label="Contraseña" name="userPassword" type="password" isRequired={true} size="sm" />
-        <Input label="Repetir contraseña" type="password" isRequired={true} size="sm" />
+    <form className="bg-orange-500 px-10 py-2 rounded-md" onSubmit={handleSubmit} >
+      <p className="text-2xl my-4 text-white">
+        Iniciar sesión
+      </p>
+      <div className="flex flex-col gap-4 my-4 items-center">
+      <Input label="Email" name="userEmail" type="email" required size="sm" />
+      <Input label="Contraseña" name="userPassword" type="password" required size="sm" />
+
       </div>
       <div className="flex flex-col items-center gap-2">
-        <Button color="primary">Iniciar Sesion</Button>
-        <p>¿No tienes cuenta? <Link href="/signup" className="text-orange-100 underline">¡Regístrate!</Link></p>
+        <Button color="primary" type="submit">Iniciar Sesión</Button>
+        <p className="text-white">
+          ¿No tienes cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link>
+        </p>
       </div>
     </form>
   );
 }
+
+
